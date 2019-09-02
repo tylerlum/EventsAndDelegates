5bdf4dd813532a0d0a1c9ca4f18a15933880efb5
Tyler Lum <tylergwlum@gmail.com>
2019-09-02 10:03:57 -0700

    Add MailService that subscribes to video encoding

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EventsAndDelegates
@@ -10,11 +11,33 @@ namespace EventsAndDelegates
     {
         static void Main(string[] args)
         {
+            // Create video and encoder
             var video = new Video() { Title = "Video 1" };
-            var videoEncoder = new VideoEncoder();
+            var videoEncoder = new VideoEncoder();  // publisher
 
+            // Create mail service
+            var mailService = new MailService();  // subscriber
+
+            // Subscribe by adding reference to mailService function
+            // Note: no brackets
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+
+            // Encode video
             videoEncoder.Encode(video);
+
+            // Wait to show output
             Console.ReadLine();
+        }
+    }
+
+    public class MailService
+    {
+        public void OnVideoEncoded(object source, EventArgs e)
+        {
+            // Send virtual mail
+            Console.WriteLine("MailService: Sending an email...");
+            Thread.Sleep(1000);
+            Console.WriteLine("MailService: Email Sent!");
         }
     }
 }
