bb1fdeb3372c0cd99de6215ad804ed31cbc9fc6f
Tyler Lum <tylergwlum@gmail.com>
2019-09-02 09:53:43 -0700

    Add simple video and videoEncoder

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "Video 1" };
+            var videoEncoder = new VideoEncoder();
+
+            videoEncoder.Encode(video);
+
+        }
+    }
+}
---
+++ VideoEncoder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    class VideoEncoder
+    {
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding video...");
+            Thread.Sleep(10000);
+            Console.WriteLine("Encoding complete!");
+        }
+    }
+}
